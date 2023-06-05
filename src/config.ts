a89a38533a08f99261052b86309a93458df1225f
Theo Hallenius <theo.hallenius@gmail.com>
2023-06-05 21:12:04 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Theo\'s Personal Website Template';
-export const SITE_DESCRIPTION = 'Theo\'s Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
+export const SITE_DESCRIPTION = 'Theo\'s Personal Website';
