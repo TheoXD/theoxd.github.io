d10d2010571295678078a125cdd8b1acaef7be5f
Theo Hallenius <theo.hallenius@gmail.com>
2023-06-05 21:34:45 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Theo\'s Personal Website Template';
+export const SITE_TITLE = 'Theo\'s Personal Website';
 export const SITE_DESCRIPTION = 'Theo\'s Personal Website';
